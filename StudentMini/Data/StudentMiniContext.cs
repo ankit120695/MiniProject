6f8e6ca272b917db3c78626c97f732c98e8aacd2
ankit120695 <ankit.thakare1995@gmail.com>
2023-02-24 12:34:12 +0530

    Myproject

---
+++ StudentMini/Data/StudentMiniContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudentMini.Model;
+
+namespace StudentMini.Data
+{
+    public class StudentMiniContext : DbContext
+    {
+        public StudentMiniContext (DbContextOptions<StudentMiniContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentMini.Model.Student> Student { get; set; } = default!;
+    }
+}
---
+++ StudentMini/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using StudentMini.Data;
+using StudentMini.Model;
+
+namespace StudentMini.Pages.Students
+{
+    public class CreateModel : PageModel
+    {
+        private readonly StudentMini.Data.StudentMiniContext _context;
+
+        public CreateModel(StudentMini.Data.StudentMiniContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Student.Add(Student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
